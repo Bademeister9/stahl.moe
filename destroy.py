063f2e21ac68e91efc573fc9cc79999c5220e5aa
Paul Stahl <info@paulstahl.com>
2024-05-19 17:20:26 +0200

    Add destroy.py
    this file removes all symlinks created by the deploy.py

---
+++ destroy.py
@@ -0,0 +1,14 @@
+from deploy import get_files, sync_dir
+import subprocess
+
+def destroy():
+    for path in sync_dir:
+        root_files = get_files(path)
+        print("Destroying {} files...".format(root_files))
+        pr = subprocess.run(['rm', root_files])
+        if pr.returncode != 0:
+            print("Failed to remove {} files.".format(root_files))
+            return False
+        else:
+            print("Successfully removed {} files.".format(root_files))
+            return True
